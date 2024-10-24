2b547a9f34cb870ab74a8f5e726067815311560a
Denys <Denys@DESKTOP-J9IAKOQ>
2024-10-24 21:30:41 +0300

    Add project files.

---
+++ ViewBagExample/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using ViewBagExample.Models;
+
+namespace ViewBagExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult CarsTable()
+        {
+            ViewBag.Cars = new List<string> { "Бразилия", "Аргентина", "Уругвай", "Чили" };
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
